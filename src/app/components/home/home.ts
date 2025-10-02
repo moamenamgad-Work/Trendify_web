6b3b1fd5756510e53b9eac41e786f3b168fa0295
moamenamgad-Work <maomenamgadw@gmail.com>
2025-10-03 01:26:20 +0300

    update

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-   standalone: true,
+  imports: [RouterLink],
+  standalone: true,
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
